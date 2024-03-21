6013233fc135f85684935f2c23cad9ab88627ecf
Mateusz Adamiec <mateusz.adamiec@a4bee.com>
2024-03-21 13:38:57 +0100

    modernize settings

--- game-problem/src/store/index.js
+++ game-problem/src/store/index.js
@@ -1,73 +1,77 @@
-import Vue from "vue"
-import Vuex from "vuex"
+import { createStore } from 'vuex';
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+export default createStore({
   state: {
-    uiState: "start",
+    uiState: 'start',
     score: 0,
-    character: "",
-    characterChoices: ["baker", "mechanic", "artist"],
+    character: '',
+    characterChoices: ['baker', 'mechanic', 'artist'],
     questionIndex: 0,
-    questions: [{
+    questions: [
+      {
         question: `What's your dog's name?`,
-        baker: "Woofgang Puck",
-        mechanic: "Alf",
-        artist: "Salvador Dogi",
+        baker: 'Woofgang Puck',
+        mechanic: 'Alf',
+        artist: 'Salvador Dogi',
       },
       {
         question: `What's your favorite hobby?`,
-        baker: "Extreme ironing",
-        mechanic: "Bacon santa cosplay",
-        artist: "Mimosas",
+        baker: 'Extreme ironing',
+        mechanic: 'Bacon santa cosplay',
+        artist: 'Mimosas',
       },
       {
         question: `What's your favorite color?`,
-        baker: "turquoise",
-        mechanic: "yellow",
-        artist: "transparent",
+        baker: 'turquoise',
+        mechanic: 'yellow',
+        artist: 'transparent',
       },
       {
         question: `Is cereal soup?`,
         baker: "You can't be serieal",
-        mechanic: "Yes, I am Jason Lengstorf",
-        artist: "wut",
+        mechanic: 'Yes, I am Jason Lengstorf',
+        artist: 'wut',
       },
       {
         question: `What’s the most imaginative insult you can come up with?`,
         baker: "You're a substitute teacher with no lesson plan",
-        mechanic: "Yer face is a melted welly",
-        artist: "You eat buttons off the remote",
+        mechanic: 'Yer face is a melted welly',
+        artist: 'You eat buttons off the remote',
       },
       {
         question: `If peanut butter wasn’t called peanut butter, what would it be called?`,
-        baker: "legoome",
-        mechanic: "brown paste",
-        artist: "groundnut smoosh",
+        baker: 'legoome',
+        mechanic: 'brown paste',
+        artist: 'groundnut smoosh',
       },
     ],
   },
   mutations: {
     updateCharacter(state, choice) {
-      state.character = choice
+      state.character = choice;
     },
     updateScore(state, amount) {
-      state.score = amount
+      state.score = amount;
     },
     updateUIState(state, uistate) {
-      state.uiState = uistate
+      state.uiState = uistate;
     },
     pickQuestion(state, character) {
-      character === state.character ? (state.score += 13) : (state.score -= 13)
+      character === state.character ? (state.score += 13) : (state.score -= 13);
 
       if (state.questionIndex < state.questions.length - 1) {
-        state.questionIndex++
+        state.questionIndex++;
       } else {
-        Math.sign(state.score) > 0 ?
-          (state.uiState = "won") :
-          (state.uiState = "lost")
+        Math.sign(state.score) > 0
+          ? (state.uiState = 'won')
+          : (state.uiState = 'lost');
       }
     },
+    resetGame(state) {
+      state.uiState = 'start';
+      state.questionIndex = 0;
+      state.character = '';
+      state.score = 0;
+    },
   },
-})
+});
